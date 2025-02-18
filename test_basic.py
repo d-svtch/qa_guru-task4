24bc2bd7309e3b2dd85ff949f37d117a0a8fc39b
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-02-18 12:21:21 +0300

    fixes

--- test_basic.py
+++ test_basic.py
@@ -1,8 +1,10 @@
-from selene import browser, command, have
+from selene import browser, command, have, command
 import os
 
 
+
 def test_full_complited_form():
+    browser.open("/automation-practice-form")
     browser.element("#firstName").type("Ivan")
     browser.element("#lastName").type("Ivanovich")
     browser.element("#userEmail").type("Ivanovich@testmail.com")
@@ -17,7 +19,7 @@ def test_full_complited_form():
     browser.element("#hobbies-checkbox-2").perform(command.js.click())
     browser.element("#uploadPicture").send_keys(os.path.abspath("images.png"))
     browser.element("#currentAddress").type("Test adress, 11/1")
-    browser.element("#state").click().element("#react-select-3-option-2").click()
+    browser.element("#state").perform(command.js.scroll_into_view).click().element("#react-select-3-option-2").click()
     browser.element("#city").click().element("#react-select-4-option-1").click()
     browser.element("#submit").click()
 
@@ -28,18 +30,22 @@ def test_full_complited_form():
 
 
 def test_only_required_data():
+    browser.open("/automation-practice-form")
     browser.element("#firstName").type("Ivan")
     browser.element("#lastName").type("Ivanovich")
     browser.element('#gender-radio-1').perform(command.js.click())
     browser.element("#userNumber").type("7999999999")
-    browser.element("#submit").click()
+    browser.element("#submit").perform(command.js.scroll_into_view).click()
 
     browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
+    browser.all("//div[@class='table-responsive']//td[2]").should(have.exact_texts('Ivan Ivanovich', '', 'Male', '7999999999', '18 February,2025', '', '', '', '', ''))
 
 
 def test_required_data_not_filled():
+    browser.open("/automation-practice-form")
     browser.element("#firstName").type("Ivan")
     browser.element("#userNumber").type("7999999999")
-    browser.element("#submit").click()
+    browser.element("#submit").perform(command.js.scroll_into_view).click()
 
     browser.element('#example-modal-sizes-title-lg').should(have.no.exact_text('Thanks for submitting the form'))
+    browser.element('#userForm').should(have.attribute('class').value('was-validated'))
