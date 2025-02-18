0523732d62b6aa5fbceddaea8111c631167c52d9
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-02-18 09:56:43 +0300

    final result

--- basic_tests.py
+++ basic_tests.py
@@ -1,7 +1,8 @@
-from selene  import browser, command
+from selene import browser, command, have
 import os
+
+
 def test_full_complited_form():
-    browser.open("/automation-practice-form")
     browser.element("#firstName").type("Ivan")
     browser.element("#lastName").type("Ivanovich")
     browser.element("#userEmail").type("Ivanovich@testmail.com")
@@ -20,8 +21,25 @@ def test_full_complited_form():
     browser.element("#city").click().element("#react-select-4-option-1").click()
     browser.element("#submit").click()
 
+    browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
+    browser.all("//div[@class='table-responsive']//td[2]").should(
+        have.exact_texts('Ivan Ivanovich', 'Ivanovich@testmail.com', 'Male', '7999999999', '28 June,1999', 'English',
+                         'Sports, Reading', 'images.png', 'Test adress, 11/1', 'Haryana Panipat'))
 
 
+def test_only_required_data():
+    browser.element("#firstName").type("Ivan")
+    browser.element("#lastName").type("Ivanovich")
+    browser.element('#gender-radio-1').perform(command.js.click())
+    browser.element("#userNumber").type("7999999999")
+    browser.element("#submit").click()
+
+    browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
 
 
+def test_required_data_not_filled():
+    browser.element("#firstName").type("Ivan")
+    browser.element("#userNumber").type("7999999999")
+    browser.element("#submit").click()
 
+    browser.element('#example-modal-sizes-title-lg').should(have.no.exact_text('Thanks for submitting the form'))
--- conftest.py
+++ conftest.py
@@ -1,2 +1,9 @@
 from selene import browser
+import pytest
 browser.config.base_url = 'https://demoqa.com'
+
+@pytest.fixture(autouse=True)
+def operations_with_browser():
+    browser.open("/automation-practice-form")
+    yield
+    browser.quit()
